ccdd8f443e9dd3dc6349d32c079edc4f4fb122b0
ikexw <ikeudy2008@gmail.com>
2026-05-20 16:28:16 -0500

    Add product: Vintage 90s Russell Baby Blue

--- js/products.js
+++ js/products.js
@@ -8,4 +8,21 @@
 
 const dropTime = null;
 
-const products = [];
+const products = [
+  {
+    "id": 1,
+    "name": "Vintage 90s Russell Baby Blue",
+    "category": "Tops",
+    "price": 40,
+    "size": "L (23x26)",
+    "image": "images/product-1779312108370-wit.jpg",
+    "images": [
+      "images/product-1779312108370-wit.jpg",
+      "images/product-1779312115390-y7y.jpg",
+      "images/product-1779312125463-zha.jpg"
+    ],
+    "featured": true,
+    "description": "Original 1990s Russell hoodie with the perfect vintage fit and slight cropped feel. Soft heavyweight fabric with a naturally worn-in look that makes it an easy spring/summer layering piece. Timeless colorway with classic fade and character you can’t really replicate on modern blanks.  \n\nPlease note: all items are vintage and may show signs of wear including fading, distressing, or minor imperfections. Refer to measurements for the most accurate fit.",
+    "published": true
+  }
+];
